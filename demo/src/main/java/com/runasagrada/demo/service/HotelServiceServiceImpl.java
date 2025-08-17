bf31d1d42ed34f5864d8e8f5c57af2eadef8e845
Gabriel Riaño <rianogabriel@javeriana.edu.co>
2025-08-17 15:13:34 -0500

    Pagina de servicio - Basica

--- demo/src/main/java/com/runasagrada/demo/service/HotelServiceServiceImpl.java
+++ demo/src/main/java/com/runasagrada/demo/service/HotelServiceServiceImpl.java
@@ -3,10 +3,12 @@ package com.runasagrada.demo.service;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.runasagrada.demo.entities.HotelService;
 import com.runasagrada.demo.repository.HotelServiceRepository;
 
+@Service
 public class HotelServiceServiceImpl implements HotelServiceService {
 
     @Autowired
