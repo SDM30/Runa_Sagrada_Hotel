e3c349faeba24de1b6ca703b8595bcaae97fd7ea
SDM30 <simondiaz@javeriana.edu.co>
2025-08-24 12:46:46 -0500

    crear cliente

--- demo/src/main/java/com/runasagrada/demo/controller/ClientController.java
+++ demo/src/main/java/com/runasagrada/demo/controller/ClientController.java
@@ -2,15 +2,21 @@ package com.runasagrada.demo.controller;
 
 import java.util.logging.Logger;
 
+import org.h2.engine.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.runasagrada.demo.entities.Client;
+import com.runasagrada.demo.entities.HotelUser;
 import com.runasagrada.demo.service.ClientService;
 import com.runasagrada.demo.service.HotelUserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/client")
@@ -20,7 +26,7 @@ public class ClientController {
     private ClientService clientService;
 
     @Autowired
-    private HotelUserService hotelUserService;
+    private HotelUserService userService;
 
     // DEBUG
     Logger logger = Logger.getLogger(ClientController.class.getName());
@@ -33,7 +39,22 @@ public class ClientController {
 
     @GetMapping("/staff")
     public String showClientsStaff(Model model) {
+        model.addAttribute("newclient", new Client());
+        model.addAttribute("newclientuser", new HotelUser());
         model.addAttribute("clients", clientService.getAllClients());
         return "staffPage";
     }
+
+    @PostMapping("/staff")
+    public String registerOrUpdateClient(@ModelAttribute("newclientuser") HotelUser newClientUser) {
+        logger.info(newClientUser.toString());
+        userService.save(newClientUser);
+
+        Client newClient = new Client();
+        newClient.setUser(newClientUser);
+        clientService.save(newClient);
+
+        return "redirect:/client/staff";
+    }
+
 }
